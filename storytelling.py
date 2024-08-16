f5d0c7a44dca7e821f759c3ff04a50e0ddfd8c7d
Rafael Maldonado <rafaelnuttimaldonado@gmail.com>
2024-08-16 02:48:56 -0300

    Storytelling

--- storytelling.py
+++ storytelling.py
@@ -1,4 +1,6 @@
 import streamlit as st
 
 def app():
-    st.write("")
+    st.title("Storytelling")
+    st.write("Para melhor compreensão da jornada do nosso entregável, segue o vídeo: ")
+    st.video("https://www.youtube.com/watch?v=7QCaVaYMT0U")
